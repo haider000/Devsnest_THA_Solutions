5fb9255faaf319f000f51da9125f09e59a57ad9a
mohd baqer haider <mohd.b.haider000786@gmail.com>
2021-07-29 23:37:23 +0530

    done day 24

--- Frontend/Day-23/src/App.js
+++ Frontend/Day-23/src/App.js
@@ -1,47 +1,13 @@
 import React, { useState } from 'react'
-import { Card, MainCard } from './Card'
+import useWindowSize from './CustomHooks/useWindowSize'
 export default function App() {
-  const [items, setItems] = useState([])
-  //   console.log(items)
-  const onEdit = (index, newVal) => {
-    setItems(
-      items.map((obj, idx) => {
-        if (idx === index) {
-          console.log(obj)
-          return { ...obj, ...newVal }
-        }
-        return obj
-      }),
-    )
-  }
+  const [width, height] = useWindowSize()
 
-  const onDelete = (index) => {
-    setItems(items.filter((_, idx) => idx !== index))
-  }
   return (
     <div className="container d-flex flex-column align-items-center">
-      <div>
-        <MainCard items={items} setItems={setItems} />
-      </div>
-
-      <div
-        className="d-flex flex-column"
-        style={{
-          justifyContent: 'center',
-          alignItems: 'center',
-          overflow: 'scroll',
-        }}
-      >
-        {items.map((item, idx) => (
-          <Card
-            key={idx}
-            item={item}
-            idx={idx}
-            onEdit={onEdit}
-            onDelete={onDelete}
-          />
-        ))}
-      </div>
+      <h1>
+        Current window size is {width}x{height}
+      </h1>
     </div>
   )
 }
